b6f294fbeb81d04cb25de7ea4f37d5ae4e97b0f5
Likhith Kanigolla <likhithkanigolla@gmail.com>
2023-12-20 16:00:40 +0530

    Removed Dragging of Markers and Added some buttons, Table which contains information of markers

--- src/App.js
+++ src/App.js
@@ -7,39 +7,43 @@ import L from 'leaflet';
 import ReactDOMServer from 'react-dom/server';
 import Swal from 'sweetalert2';
 import axios from 'axios';
+import './App.css';
 
+const dt_node_1 = [17.44773337470836, 78.34853368169597];
+const dt_node_2 = [17.44711288989055, 78.34927584903512];
+const dt_node_3 = [17.446087802969153, 78.35051801020884];
 
-const position1 = [17.44774477348508, 78.3485408555236];
-const position2 = [17.446987521805, 78.34955172607279];
-const position3 = [17.44626262836117, 78.35030479071011];
-
-const mapPosition = [17.44645, 78.34891];
+const mapPosition = [17.44695, 78.34891];
 
 const data = [
   {
     "Node ID": "Node-1",
-    "Node Location": [17.44774477348508, 78.3485408555236],
+    "Node Location": [17.44773337470836, 78.34853368169597],
     "Data String Parameters": [
-      "Flowrate",
-      "Total Flow"
+      "Temperature", 
+      "Uncompensated_TDS" , 
+      "Compensated_TDS", 
+      "Voltage_TDS"
     ]
   },
   {
     "Node ID": "Node-2",
-    "Node Location": [17.446987521805, 78.34955172607279],
+    "Node Location": [17.44711288989055, 78.34927584903512],
     "Data String Parameters": [
-      "Flow Volume",
-      "Flow Time",
-      "Flow Rate"
+      "Temperature", 
+      "Uncompensated_TDS" , 
+      "Compensated_TDS", 
+      "Voltage_TDS"
     ]
   },
   {
     "Node ID": "Node-3",
-    "Node Location": [17.44626262836117, 78.35030479071011],
-    "Data String Parameters": [ 
-      "Flow Volume",
-      "Flow Time",
-      "Flow Rate"
+    "Node Location": [17.446087802969153, 78.35051801020884],
+    "Data String Parameters": [
+      "Temperature", 
+      "Uncompensated_TDS" , 
+      "Compensated_TDS", 
+      "Voltage_TDS"
     ]
   }
 ];
@@ -52,126 +56,40 @@ function App() {
   const [clickedLatLng, setClickedLatLng] = useState(null);
   const [clickedCoordinates, setClickedCoordinates] = useState(null);
 
-// ... (rest of your code)
+  const [latitudeInput, setLatitudeInput] = useState('17.447356');
+  const [longitudeInput, setLongitudeInput] = useState('78.349047');
+  const [selectedMarkerIndex, setSelectedMarkerIndex] = useState(null);
 
-
-
-  const addMarker = () => {
-    const newMarker = {
-      position: [17.4474, 78.3491],
-      flowrate: 0,
-      totalflow: 0,
-    };
-    setMarkers([...markers, newMarker]);
-    setIsAddingMarker(true);
-  };
-
-// const handleMapClick = (e) => {
-
-//   const latitude = e.latlng.lat.toFixed(6); // Format latitude with 6 decimal places
-//   const longitude = e.latlng.lng.toFixed(6); // Format longitude with 6 decimal places
-//   const content = `You clicked at: Latitude: ${latitude}, Longitude: ${longitude}`;
-//   console.log("Event Called:");
-//   console.log(content);
-
-//   setClickedLatLng({ latitude, longitude });
-
-//   // Ensure that mapRef.current is defined before accessing leafletElement
-//   if (mapRef.current && mapRef.current.leafletElement) {
-//     L.popup()
-//       .setLatLng(e.latlng)
-//       .setContent(content)
-//       .openOn(mapRef.current.leafletElement);
-//   }
-
-//   // if (isAddingMarker) {
-//   //   setMarkers((prevMarkers) => {
-//   //     const newMarker = {
-//   //       position: [e.latlng.lat, e.latlng.lng],
-//   //       flowrate: 0,
-//   //       totalflow: 0,
-//   //     };
-//   //     setIsAddingMarker(false);
-//   //     return [...prevMarkers, newMarker];
-//   //   });
-//   // }
-// };
-
-const handleMapClick = (e) => {
-  const latitude = e.latlng.lat.toFixed(6);
-  const longitude = e.latlng.lng.toFixed(6);
-  const content = `You clicked at: Latitude: ${latitude}, Longitude: ${longitude}`;
-  console.log("Event Called:");
-  console.log(content);
-
-  setClickedLatLng({ latitude, longitude });
-
-  // Ensure that mapRef.current is defined before accessing leafletElement
-  if (mapRef.current && mapRef.current.leafletElement) {
-    L.popup()
-      .setLatLng(e.latlng)
-      .setContent(content)
-      .openOn(mapRef.current.leafletElement);
-  }
-};
-
-
-  
-
-  const mapRef = useRef(); 
-  // const mapRef = null;
-
-  const handleMarkerDragEnd = (index, event) => {
-    const updatedMarkers = [...markers];
-    const { lat, lng } = event.target._latlng;
-    updatedMarkers[index].position = [lat, lng];
-    setMarkers(updatedMarkers);
-  };
-
-  const isPointNearPolyline = (point, polyline, tolerance = 0.0005) => {
-    for (let i = 0; i < polyline.length - 1; i++) {
-      const segmentStart = L.latLng(polyline[i]);
-      const segmentEnd = L.latLng(polyline[i + 1]);
-      const distanceToSegment = point.distanceToSegment(segmentStart, segmentEnd);
-
-      if (distanceToSegment < tolerance) {
-        return true;
-      }
-    }
-
-    return false;
-  };
+  const mapRef = useRef();
 
   const clearMarkers = () => {
     setMarkers([]);
   };
 
-  const handleMarkerClick = (index) => {
-    const marker = markers[index];
-    if (marker) {
-      const updatedPosition = marker.position;
-      const updatedMarkers = markers.map((m, i) => {
-        if (i === index) {
-          return { ...m, position: [updatedPosition[0], updatedPosition[1]] };
-        }
-        return m;
-      });
+  const handleRemoveMarker = (index) => {
+    if (index !== null) {
+      const updatedMarkers = markers.filter((_, i) => i !== index);
       setMarkers(updatedMarkers);
-      setClickedNode(marker);
+      setSelectedMarkerIndex(null);
     }
   };
 
-  // const handleMarkerClick = (index) => {
-  //   console.log("HandleMarkerClick is called")
-  //   const marker = markers[index];
-  //   if (marker) {
-  //     const lat = marker.position[0].toFixed(6);
-  //     const lng = marker.position[1].toFixed(6);
-  //     setClickedLatLng({ latitude: lat, longitude: lng });
-  //     setClickedCoordinates({ latitude: lat, longitude: lng });
-  //   }
-  // };
+  const handleMapClick = (e) => {
+    const latitude = e.latlng.lat.toFixed(6);
+    const longitude = e.latlng.lng.toFixed(6);
+    setClickedLatLng({ latitude, longitude });
 
+    if (latitudeInput && longitudeInput) {
+      const newMarker = {
+        position: [parseFloat(latitudeInput), parseFloat(longitudeInput)],
+        flowrate: 0,
+        totalflow: 0,
+      };
+      setMarkers([...markers, newMarker]);
+      setLatitudeInput('');
+      setLongitudeInput('');
+    }
+  };
 
   const logMarkerCoordinates = () => {
     markers.forEach((marker, index) => {
@@ -179,13 +97,50 @@ const handleMapClick = (e) => {
     });
   };
 
+  const buildPopupContent = (index) => {
+    const marker = markers[index];
+    if (marker && clickedLatLng) {
+      return (
+        <div>
+          {`Marker ${index + 1} - Clicked Coordinates:`}
+          <br />
+          Latitude: {clickedLatLng.latitude}
+          <br />
+          Longitude: {clickedLatLng.longitude}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          {`Marker ${index + 1} - Coordinates:`}
+          <br />
+          Latitude: {marker.position[0].toFixed(6)}
+          <br />
+          Longitude: {marker.position[1].toFixed(6)}
+        </div>
+      );
+    }
+  };
+
+  const addMarker = () => {
+    if (latitudeInput && longitudeInput) {
+      const newMarker = {
+        position: [parseFloat(latitudeInput), parseFloat(longitudeInput)],
+        flowrate: 0,
+        totalflow: 0,
+      };
+      setMarkers([...markers, newMarker]);
+      setLatitudeInput((prev) => (prev === '' ? '' : (parseFloat(prev)).toString()));
+      setLongitudeInput((prev) => (prev === '' ? '' : (parseFloat(prev)).toString()));
+    }
+  };
+
   useEffect(() => {
     const map = mapRef.current;
     if (map) {
-      console.log("I am clicked");
       map.on('click', handleMapClick);
     }
-  
+
     const nodes = ["Node-1", "Node-2"];
     for (let i = 0; i < nodes.length; i++) {
       let url = "http://localhost:8080/desc/" + nodes[i];
@@ -193,89 +148,7 @@ const handleMapClick = (e) => {
         data[nodes[i]] = response.data;
       });
     }
-  }, [handleMapClick]); 
-
-  const handlePopupClick = (index, e) => {
-    console.log("handlePopupClick is called when marker clicked");
-    const marker = markers[index];
-    if (marker) {
-      const lat = e.latlng.lat.toFixed(6);
-      const lng = e.latlng.lng.toFixed(6);
-      setClickedLatLng({ latitude: lat, longitude: lng });
-    }
-  };
-
-  const buildPopupContent = (index) => {
-    console.log("I am called buildPopupContent");
-    const marker = markers[index];
-    if (marker && clickedLatLng) {
-      return (
-        <div>
-          {`Marker `}
-          {index + 1}
-          {`- Clicked Coordinates:`}
-          <br />
-          Latitude: {clickedLatLng.latitude}
-          <br />
-          Longitude: {clickedLatLng.longitude}
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          {`Marker `}
-          {index + 1}
-          {`- Default Coordinates:`}
-          <br />
-          Latitude: {marker.position[0].toFixed(6)}
-          <br />
-          Longitude: {marker.position[1].toFixed(6)}
-        </div>
-      );; // Or some default content
-    }
-  };
-  
-
-  // const buildPopupContent = () => {
-  //   console.log("I am called buildPopupContent");
-  //   handlePopupClick()
-  //   if (clickedCoordinates) {
-  //     return (
-  //       <div>
-  //         {`Clicked Coordinates:`}
-  //         <br />
-  //         Latitude: {clickedCoordinates.latitude}
-  //         <br />
-  //         Longitude: {clickedCoordinates.longitude}
-  //       </div>
-  //     );
-  //   } else {
-  //     return (
-
-  //       <div>
-  //         Got Blank Values 
-  //       </div>
-  //     ); // Or some default content
-  //   }
-  // };
-
-
-  const getPopupContent = () => {
-    if (clickedLatLng) {
-      const content = (
-        <div>
-          {`- Clicked Coordinates:`}
-          <br />
-          Latitude: {clickedLatLng.latitude}
-          <br />
-          Longitude: {clickedLatLng.longitude}
-        </div>
-      );
-      return ReactDOMServer.renderToString(content);
-    } else {
-      return null;
-    }
-  };
+  }, []);
 
   return (
     <div>
@@ -285,11 +158,9 @@ const handleMapClick = (e) => {
           ref={mapRef}
           center={mapPosition}
           zoom={18}
-          style={{ maxWidth: '100%', height: '80vh' }}
+          style={{ maxWidth: '100%', height: '60vh' }}
           scrollWheelZoom={false}
-          onClick={handleMapClick}
         >
-
           <TileLayer url='https://tile.openstreetmap.org/{z}/{x}/{y}.png' />
           {data.map((nodeData, index) => (
             <CustomCircleMarker
@@ -302,9 +173,6 @@ const handleMapClick = (e) => {
             <Marker
               key={index}
               position={marker.position}
-              draggable={true}
-              onDragEnd={(event) => handleMarkerDragEnd(index, event)}
-              onClick={() => handleMarkerClick(index)}
               icon={
                 new L.divIcon({
                   className: 'custom-icon',
@@ -313,17 +181,59 @@ const handleMapClick = (e) => {
                 })
               }
             >
-               <Popup>{buildPopupContent(index)}</Popup>
-               {/* <Popup>{handleMapClick(index)}</Popup> */}
+              <Popup>{buildPopupContent(index)}</Popup>
             </Marker>
           ))}
-          <Polyline pathOptions={{ color: 'green', dashArray: '5' }} positions={[position1, position2, position3]} />
+          <Polyline pathOptions={{ color: 'green', dashArray: '5' }} positions={[dt_node_1, dt_node_2, dt_node_3]} />
         </MapContainer>
       </div>
-      <div style={{ textAlign: 'center' }}>
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        <div>
+          <label>Latitude:</label>
+          <input
+            type="number"
+            value={latitudeInput}
+            // defaultValue={17.447356}
+            onChange={(e) => setLatitudeInput(e.target.value)}
+          />
+
+          <label>Longitude:</label>
+          <input
+            type="number"
+            value={longitudeInput}
+            // defaultValue={78.349047}
+            onChange={(e) => setLongitudeInput(e.target.value)}
+          />
+        </div>
+        <button onClick={() => handleRemoveMarker(selectedMarkerIndex)}>Remove Marker</button>
         <button onClick={addMarker}>Add Marker</button>
         <button onClick={clearMarkers}>Clear Markers</button>
         <button onClick={logMarkerCoordinates}>Log Marker Coordinates</button>
+
+        <table style={{ width: '100%' }}>
+          <thead>
+            <tr>
+              <th>Marker Name</th>
+              <th>Latitude</th>
+              <th>Longitude</th>
+              <th>Remove Marker</th>
+            </tr>
+          </thead>
+          <tbody>
+            {markers.map((marker, index) => (
+              <tr key={index}>
+                <td>Marker {index + 1}</td>
+                <td>{marker.position[0].toFixed(6)}</td>
+                <td>{marker.position[1].toFixed(6)}</td>
+                <td>
+                  <button onClick={() => handleRemoveMarker(index)}>
+                    Remove Marker
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
